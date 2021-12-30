e5d89d83dbc725516928f669b9a63623d529da66
Dustin Knopoff <dustin@knopoff.dev>
2021-12-30 21:27:38 +0000

    annotate

--- src/main.rs
+++ src/main.rs
@@ -33,28 +33,48 @@ fn main() -> anyhow::Result<()> {
     };
 
     // Calculate
+    // Initializations
     let mut cummulative_rate = 0_f32;
     let max_capacity = all_expenses.len();
     let mut rates: Vec<(f32, f32)> = Vec::with_capacity(max_capacity - 1);
     let mut avg_daily_expense = 0_f32;
     let mut avg_daily_income = 0_f32;
+    // Skips account name column and total column
+    // all_expenses.len() - 1 would be the last in the list
     let num_months = all_expenses.len() - 2;
+    // We've prefiltered to remove all but the last row
+    // "account","2021-07","2021-08","2021-09","2021-10","2021-11","2021-12","total"
+    // "Expenses:1","0","0","0","0","0","16.50 USD","16.50 USD"
+    // "Expenses:2","0","0","0","0","0","30.00 USD","30.00 USD"
+    // "Expenses:3","0","0","0","0","0","350.00 USD","350.00 USD"
+    // "total","0","0","0","0","0","396.50 USD","396.50 USD"
+    //index: 0                                  index: len -1
+    // This is filtered to be on the `total` row. For each month
     for (index, value) in all_expenses.iter().enumerate() {
-        // Columns for each month, skipping first column (account name) and last (total)
-        // Get as numbers. Equation is (income - expenses) / income
+        // skipping first column (account name) and last (total)
         if index != 0 && index <= num_months {
+            // My USD are reported as 1,000.00 USD,
+            // `string_to_f32` strips the comma and parses as a float
             let expenses: f32 = string_to_f32(value)?;
+            // Grab the same index from the `Income` query and parse to float
             let income: f32 = string_to_f32(all_income.iter().nth(index).unwrap())?;
+            // Calculate rate
             let rate = (income - expenses) / income;
+            // If it's NaN, return 0
             let rate = if rate.is_nan() { 0_f32 } else { rate };
+            // Push to our List for displaying as a step graph
             rates.push(((index - 1) as f32, rate * 100_f32));
+            // Sum average daily expenses and income hard coded to a month as 30 days
             avg_daily_expense += expenses / 30_f32;
             avg_daily_income += income / 30_f32;
+            // Add to cummulative_rate
             cummulative_rate += rate;
         }
     }
+    // Calculate average daily income over months in this + last quarter
     avg_daily_income /= (num_months) as f32;
     avg_daily_expense /= (num_months) as f32;
+    // calculate average savgins rate over months in this + last quarter
     cummulative_rate /= (num_months) as f32;
     let fire = avg_daily_expense * 365_f32 * 25_f32;
     let aaw = ((avg_daily_income * 365_f32 * 23_f32) / 10_f32 / 2_f32 - liabilities).abs();
@@ -62,7 +82,15 @@ fn main() -> anyhow::Result<()> {
 
     let expenses_breakdown = fetch_expenses_this_month()?;
     // Get final row/column as number
+    // "account","balance"
+    // "Expenses:1","16.50 USD"
+    // "Expenses:2","30.00 USD"
+    // "Expenses:3","350.00 USD"
+    // "total","396.50 USD"
+    //         ^ uses this number
     let total: f32 = string_to_f32(expenses_breakdown.last().unwrap().iter().last().unwrap())?;
+    // Skipping the account name column,
+    // create the data structure for our piechart
     let data: Vec<piechart::Data> = expenses_breakdown[..expenses_breakdown.len() - 1]
         .iter()
         .enumerate()
