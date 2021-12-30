be4bd8aaa7e9aab572ed19bcb2d79685b96f6854
Dustin Knopoff <dustin@knopoff.dev>
2021-12-30 20:53:27 +0000

    a small bit of organizing

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use textplots::{Chart, Plot, Shape};
 use xshell::cmd;
 
 fn main() -> anyhow::Result<()> {
+    // Constants
     let fills = ['•', '▪', '▴', '░', '▀'];
     let colors = [
         Color::Blue,
@@ -13,6 +14,8 @@ fn main() -> anyhow::Result<()> {
         Color::Yellow,
         Color::Purple,
     ];
+
+    // Fetch via hledger
     let liabilities = fetch_liabilities()?;
     let all_expenses = fetch_balance("Expenses")?;
     // Fetch only total;
@@ -28,6 +31,8 @@ fn main() -> anyhow::Result<()> {
         Some(val) => val,
         None => anyhow::bail!("No income recorded. Calculating savings rate relies on comparisons between income and expenses.")
     };
+
+    // Calculate
     let mut cummulative_rate = 0_f32;
     let max_capacity = all_expenses.len();
     let mut rates: Vec<(f32, f32)> = Vec::with_capacity(max_capacity - 1);
@@ -55,10 +60,6 @@ fn main() -> anyhow::Result<()> {
     let aaw = ((avg_daily_income * 365_f32 * 23_f32) / 10_f32 / 2_f32 - liabilities).abs();
     let paw = (((avg_daily_income * 365_f32 * 23_f32) / 10_f32) * 2_f32 - liabilities).abs();
 
-    println!("savings rate: last {} months", num_months);
-    Chart::new(120, 60, 0.0, 8.0)
-        .lineplot(&Shape::Steps(&rates))
-        .nice();
     let expenses_breakdown = fetch_expenses_this_month()?;
     // Get final row/column as number
     let total: f32 = string_to_f32(expenses_breakdown.last().unwrap().iter().last().unwrap())?;
@@ -73,6 +74,12 @@ fn main() -> anyhow::Result<()> {
             color: Some(colors[index % 5].into()),
         })
         .collect();
+
+    // Present
+    println!("savings rate: last {} months", num_months);
+    Chart::new(120, 60, 0.0, 8.0)
+        .lineplot(&Shape::Steps(&rates))
+        .nice();
     println!(
         "Average Savings Rate: {}",
         current_rate_colored(cummulative_rate * 100_f32)
@@ -90,14 +97,13 @@ fn main() -> anyhow::Result<()> {
 
 fn fetch_balance(account_prefix: &str) -> anyhow::Result<Vec<StringRecord>> {
     let output =
-        cmd!("hledger bal ^{account_prefix} -T -M -X USD -C -U --begin lastquarter -O csv")
-            .read()?;
+        cmd!("hledger bal ^{account_prefix} -O csv -M -b lastquarter -C -U -T -X USD").read()?;
     let mut rdr = Reader::from_reader(output.as_bytes());
     Ok(rdr.records().filter_map(|record| record.ok()).collect())
 }
 
 fn fetch_expenses_this_month() -> anyhow::Result<Vec<StringRecord>> {
-    let output = cmd!("hledger bal ^Expenses  --begin lastquarter -O csv -X USD").read()?;
+    let output = cmd!("hledger bal ^Expenses  --begin thismonth -O csv -X USD").read()?;
     let mut rdr = Reader::from_reader(output.as_bytes());
     Ok(rdr.records().filter_map(|record| record.ok()).collect())
 }
